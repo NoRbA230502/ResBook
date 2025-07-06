96f4a2225705902965158a1f03b60643e8ee90dc
NoRbA230502 <157219502+NoRbA230502@users.noreply.github.com>
2025-07-06 17:29:17 +0200

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.reservation-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Funkcja wyszukiwania restauracji jest w przygotowaniu.');
+});
